f35ad9f399e8d04137bf2e89de27310eaef666e1
RobinBachus <robinbachus@gmail.com>
2023-04-11 11:40:48 +0200

    Add project files.

---
+++ WebUtils/DownloadUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace OsuPrune.WebUtils
+{
+    internal class DownloadUtils
+    {
+        public static async Task Download(string url, string localPath)
+        {
+
+            using HttpClient client = new();
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                using Stream stream = await response.Content.ReadAsStreamAsync();
+                using FileStream fileStream = new(localPath, FileMode.Create, FileAccess.Write);
+                await stream.CopyToAsync(fileStream);
+            }
+            else
+            {
+                Console.WriteLine($"Failed to download file. Status code: {response.StatusCode}");
+            }
+
+        }
+    }
+}
