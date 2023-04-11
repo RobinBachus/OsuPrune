4e9e282864e3609c293de455fb399b9041fa05c3
RobinBachus <robinbachus@gmail.com>
2023-04-11 11:44:04 +0200

    Revert "Revert "Add project files.""
    This reverts commit 7bfa5ac0e4c4a08136be1b1a2e0648a88c6f00d3.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using OsuPrune.Beatmaps;
+using System.Runtime.CompilerServices;
+using static OsuPrune.Common;
+
+[assembly: InternalsVisibleTo("OsuPrune_UnitTest")]
+
+namespace OsuPrune
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSets SortedMaps = new();
+        }
+    }
+}
